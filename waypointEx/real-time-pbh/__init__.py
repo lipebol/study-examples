dc8b9283b57392afbd8fffb983597e1afb7fa964
lipebol <lipe_bol5@hotmail.com>
2026-09-08 20:55:09 -0300

    moved to 'archived'

--- waypointEx/real-time-pbh/__init__.py
+++
@@ -1,6 +0,0 @@
-from common.utilize import Use
-
-Use.variable('DATARIZED_CORE_NAME', add='real-time-pbh')
-Use.variable('DATARIZED_CORE_VERSION', add='v1')
-
-setattr(Use, 'this_path', Use.path(Use.path(), join=['waypointEx','real-time-pbh']))
